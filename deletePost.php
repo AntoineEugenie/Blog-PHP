9eaf52530348dc30ad8a80b591ebd96806e37e15
Dilion <antoine.eugenie@ynov.com>
2025-01-07 14:15:26 +0100

    Update deletePost.php

--- deletePost.php
+++ deletePost.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = intval($_POST['id']);
     $stmt = $con->prepare('DELETE FROM posts WHERE ID= (?)');
     $stmt->execute([$id]);
-    $stmt2 = $con->prepare('DELETE FROM comments WHERE Post_ID= (?)');
+    $stmt2 = $con->prepare('DELETE FROM comments WHERE ID_Post= (?)');
     $stmt2->execute([$id]);
 }
 header("Location: admin.php");
